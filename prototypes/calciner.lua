ce573b5da2a36303bbff9ad631fb2996a4e8f7f6
Talandar <talandar66@protonmail.com>
2026-07-24 12:14:30 +0200

    diesel engine flag change

--- prototypes/calciner.lua
+++ prototypes/calciner.lua
@@ -163,7 +163,7 @@ data:extend({
 		result_inventory_size = 1,
 		source_inventory_size = 1,
 		crafting_speed = 4,
-		make_space_diesel_variant = true,
+		diesel_fuel_fluid_filter = true,
 		energy_usage = "1MW",
 		energy_source = {
 			type = "fluid",
